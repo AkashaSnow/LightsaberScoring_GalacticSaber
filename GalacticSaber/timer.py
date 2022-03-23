2d67bcc2f75549baad74c21061d15ca514b4e61d
Sebrina Iford <siford04@gmail.com>
2022-03-23 13:48:04 -0500

    test

---
+++ GalacticSaber/timer.py
@@ -0,0 +1,14 @@
+import time
+
+def timer():
+    timeElapsed = 0
+    while True:
+        pass
+        print (180 - timeElapsed)
+        
+        timeElapsed += 1
+
+        if timeElapsed >= 7:
+            break
+        
+timer()
