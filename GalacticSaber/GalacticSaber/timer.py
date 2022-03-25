c56ff89b3868bf8da07c7e5d6c93f4f71cbef8bf
Sebrina Iford <siford04@gmail.com>
2022-03-25 13:06:02 -0500

    timer-python

--- GalacticSaber/GalacticSaber/timer.py
+++ GalacticSaber/GalacticSaber/timer.py
@@ -1,32 +1,13 @@
+#Importing the time
 import time
-import datetime
-
-# Create class that acts as a countdown
-def countdown(h, m, s):
-
-    # Calculate the total number of seconds
-    total_seconds = h * 3600 + m * 60 + s
-
-    # While loop that checks if total_seconds reaches zero
-    # If not zero, decrement total time by one second
-    while total_seconds > 0:
-
-        # Timer represents time left on countdown
-        timer = datetime.timedelta(seconds = total_seconds)
-
-        # Prints the time left on the timer
+#The actual timer itself
+def countdown(t):
+    while t:
+        mins, secs = divmod(t,60)
+        timer = '{:02d} : {:02d}'.format(mins,secs)
         print(timer, end="\r")
-
-        # Delays the program one second
         time.sleep(1)
-
-        # Reduces total time by one second
-        total_seconds -= 1
-
-    print("End of Match")
-
-# Inputs for hours, minutes, seconds on timer
-h = input("Enter the time in hours: ")
-m = input("Enter the time in minutes: ")
-s = input("Enter the time in seconds: ")
-countdown(int(h), int(m), int(s)) 
+        t -= 1
+    print('End of Match')
+#Change this to change the set time of match
+countdown(180)
