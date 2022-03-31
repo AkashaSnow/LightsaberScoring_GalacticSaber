fafa7c6e82b846e5e7d61712bbec2f32f8b05ba9
Akasha <snow.akasha@gmail.com>
2022-03-31 12:40:08 -0500

    gh

--- GalacticSaber/matchscoring/views.py
+++ GalacticSaber/matchscoring/views.py
@@ -1,10 +1,6 @@
 from re import template
 from django.shortcuts import render
 from django.views import generic
-
-
-# Create your views here.
-
 from django.http import HttpResponse
 
 from .models import Score
