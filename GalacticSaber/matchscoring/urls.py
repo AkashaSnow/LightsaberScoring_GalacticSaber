81b626ecbcadb67915a3f137b5090c46b87fecf0
Sebrina Iford <siford04@gmail.com>
2022-04-06 12:32:18 -0500

    timer

--- GalacticSaber/matchscoring/urls.py
+++ GalacticSaber/matchscoring/urls.py
@@ -1,15 +1,12 @@
+from threading import Timer
 from django.urls import path
 
 from . import views
 
 app_name = 'matchscoring'
 urlpatterns = [
-    path('score', views.ScoreView.as_view(), name='score'),
-    path('timer', views.timer), 
+    path('', views.ScoreView.as_view(), name='score'),
+    path('timer', views.timer, name= 'timer'), 
 
     ]
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e61188568351922e7af00ea2466b4edf4224ddab
